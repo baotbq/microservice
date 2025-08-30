fd57d2fbbb0ac651c127757099b91c3ed4f3a807
baotbq <quocbaothaibinh@gmail.com>
2025-08-30 23:50:55 +0700

    auth-service-20250830

--- auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -2,11 +2,8 @@ package com.example.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableConfigServer
+@SpringBootApplication
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
