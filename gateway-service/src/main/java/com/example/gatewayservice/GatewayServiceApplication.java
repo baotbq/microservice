e29b03d681eb203c7b33c1161491914b358ae4a7
Bảo Quốc <quocbaothaibinh@gmail.com>
2025-09-04 21:56:26 +0700

    auth final

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,11 +2,9 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableEurekaClient
 @ComponentScan(basePackages = "com.example")
 public class GatewayServiceApplication {
     public static void main(String[] args) {
